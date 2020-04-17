aadddff804d957e7974be7d4ee2088f0145181f6
caujoeng <carlylezqy@qq.com>
2020-04-17 11:43:49 +0900

    123

--- Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/lib/extract_patches.py
+++ Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/lib/extract_patches.py
@@ -24,7 +24,7 @@ def get_data_training(DRIVE_train_imgs_original,
                       inside_FOV):
     train_imgs_original = load_hdf5(DRIVE_train_imgs_original)
     train_masks = load_hdf5(DRIVE_train_groudTruth) #masks always the same
-    visualize(group_images(train_imgs_original[0:20,:,:,:],5),'imgs_train').show()  #check original imgs train
+    #visualize(group_images(train_imgs_original[0:20,:,:,:],5),'imgs_train').show()  #check original imgs train
 
 
     train_imgs = my_PreProc(train_imgs_original)
--- Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/main.py
+++ Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/main.py
@@ -8,29 +8,28 @@
 ##################################################
 
 
-import numpy as np
-import os
 import configparser
-
-from keras.models import Model
-from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, UpSampling2D, Reshape, core, Dropout
-from keras.optimizers import Adam
-
-
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as K
-from keras.utils.vis_utils import plot_model as plot
-from keras.optimizers import SGD
-
+import os
 import sys
-sys.path.insert(1, '/Volumes/TimeMachine/Github/Machine_Learning/Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/lib')
-from pandas import merge
 
-from help_functions import *
-
+import keras.backend.tensorflow_backend as tfback
+import numpy as np
+import tensorflow as tf
+from keras import backend as K
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
+from keras.layers import (Conv2D, Dropout, Input, MaxPooling2D, Reshape,
+                          UpSampling2D, concatenate, core)
 # function to obtain data for training/testing (validation)
 from keras.layers.convolutional import Convolution2D
+from keras.models import Model
+from keras.optimizers import SGD, Adam
+from keras.utils.vis_utils import plot_model as plot
+from pandas import merge
+
+sys.path.insert(1, '/Volumes/TimeMachine/Github/Machine_Learning/Deep_Learning/Neural_Network/Object/item_3_U-Net/Keras/lib')
+
 from extract_patches import get_data_training
+from help_functions import *
 
 
 dir = os.path.dirname(os.path.realpath(__file__))
@@ -38,7 +37,6 @@ dir = os.path.dirname(os.path.realpath(__file__))
 
 ###############
 
-import os 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
 '''
@@ -50,8 +48,6 @@ this TensorFlow binary was not compiled to use: AVX2 FMA
 
 
 ################
-import tensorflow as tf
-import keras.backend.tensorflow_backend as tfback
 
 def _get_available_gpus():
     """Get a list of available gpu devices (formatted as strings).
@@ -204,23 +200,26 @@ patches_imgs_train, patches_masks_train = get_data_training(
     # select the patches only inside the FOV  (default == True)
 )
 
+
 # ========= Save a sample of what you're feeding to the neural network ==========
+'''
 N_sample = min(patches_imgs_train.shape[0], 40)
 visualize(group_images(patches_imgs_train[0:N_sample, :, :, :], 5),
-          './' + name_experiment + '/' + "sample_input_imgs")  # .show()
+          './' + name_experiment + '/' + "sample_input_imgs").show()
 visualize(group_images(patches_masks_train[0:N_sample, :, :, :], 5),
-          './' + name_experiment + '/' + "sample_input_masks")  # .show()
-
+          './' + name_experiment + '/' + "sample_input_masks").show()
+'''
 # =========== Construct and save the model arcitecture =====
 n_ch = patches_imgs_train.shape[1]
 patch_height = patches_imgs_train.shape[2]
 patch_width = patches_imgs_train.shape[3]
+
 model = get_unet(n_ch, patch_height, patch_width)  # the U-net model
 print("Check: final output of the network:")
 print(model.output_shape)
-plot(model, to_file='./' + name_experiment + '/' + name_experiment + '_model.png')  # check how the model looks like
+#plot(model, to_file='./' + name_experiment + '/' + name_experiment + '_model.png')  # check how the model looks like
 json_string = model.to_json()
-open('./' + name_experiment + '/' + name_experiment + '_architecture.json', 'w').write(json_string)
+#open('./' + name_experiment + '/' + name_experiment + '_architecture.json', 'w').write(json_string)
 
 # ============  Training ==================================
 checkpointer = ModelCheckpoint(filepath='./' + name_experiment + '/' + name_experiment + '_best_weights.h5', verbose=1,
@@ -236,10 +235,11 @@ checkpointer = ModelCheckpoint(filepath='./' + name_experiment + '/' + name_expe
 #
 # lrate_drop = LearningRateScheduler(step_decay)
 print("Final Check")
+'''
 patches_masks_train = masks_Unet(patches_masks_train)  # reduce memory consumption
 model.fit(patches_imgs_train, patches_masks_train, nb_epoch=N_epochs, batch_size=batch_size, verbose=2, shuffle=True,
           validation_split=0.1, callbacks=[checkpointer])
-
+'''
 # ========== Save and test the last model ===================
 model.save_weights('./' + name_experiment + '/' + name_experiment + '_last_weights.h5', overwrite=True)
 # test the model
