f0812f88c994f95de95240642d8a7710ff0909c2
CauJoeng <carlylezqy@qq.com>
2021-01-29 20:28:21 +0900

    new

--- Deep_Learning/DeepLearning_with_PyTorch_Tutorials/7.54.1_Visdom可视化.py
+++
@@ -1,5 +0,0 @@
-from visdom import Visdom
-
-viz = Visdom()
-viz.line([0.], [0.], win='train_loss', opts=dict(title='train_loss'))
-viz.line([loss.item()], [global_step], win="train_loss", update="append")
